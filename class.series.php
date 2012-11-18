da7a36e93c6a4cb8fbd929ce4a549dff9b456cef
Ajith Sebastian <ajiths701@gmail.com>
2012-11-18 23:08:14 +0800

    Provided codition to display Bazz and skip the next number

--- class.series.php
+++ class.series.php
@@ -17,18 +17,32 @@ class number_series{
 	Eg: function(12,15)
 	Output : Fizz 13 14 FizzBuzz
 	*/
-	public function fizzBuzzSeries($start_no, $end_no,$type){
+	public function fizzBuzzBazzSeries($start_no, $end_no, $type){
 		
 		//looping the integer range
 		for($i=$start_no;$i<=$end_no;$i++){
 			
-			//integrer divide by 3; print Fizz and increment the status flag to 1.
+			//set status to 0 on each loop
+			$status_flag = 0;
+			//integrer divide by 3; print Fizz and set the status flag to 1.
 			if($i%3 == 0){
-			 print "Fizz ";
-			 
+			 	print "Fizz ";
+				$status_flag = 1;
 			 }
-			if($i%5 == 0) print "Buzz ";
-			else print $i." ";
+			//integrer divide by 5; print Buzz and set the status flag to 2. 
+			if($i%5 == 0){
+				print "Buzz ";
+				$status_flag = 2;
+			}
+			//if type = 2 & status = 2 then print Bazz and skip the integer.
+			//else number.
+			//This section use to print series 2
+			if($type == 2 && $status_flag == 2){
+				print "Bazz ";
+				$i= $i+1;
+			}elseif ($status_flag == 0){
+				print $i." ";
+			} 
 	
 		}
 	}
