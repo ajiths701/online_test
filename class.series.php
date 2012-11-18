2027ffcbdf94aef60f6b656e488aa384137ad6ee
Ajith Sebastian <ajiths701@gmail.com>
2012-11-18 22:14:27 +0800

    Function:- fizzBuzzBazzSeries defined with three parameters, wwhich are start_no,end_no,type. Type is use to select series to display. Type = 1 => FizzBuzz; type=2 => FizzBuzzBazz.

--- class.series.php
+++ class.series.php
@@ -10,9 +10,27 @@ Version			: version 1.00
 
 class number_series{
 
+	/*
+	Purpose 	: Function used to display Fizz Buzz series for an integer range 
+	Parameters 	: starting interger,ending integer,type(type = 1 = > FizzBuzz Series; type = 2 = > FizzBuzzBazz Series )
 	
-	public function fizzBuzzSeries(){
+	Eg: function(12,15)
+	Output : Fizz 13 14 FizzBuzz
+	*/
+	public function fizzBuzzSeries($start_no, $end_no,$type){
+		
+		//looping the integer range
+		for($i=$start_no;$i<=$end_no;$i++){
+			
+			//integrer divide by 3; print Fizz and increment the status flag to 1.
+			if($i%3 == 0){
+			 print "Fizz ";
+			 
+			 }
+			if($i%5 == 0) print "Buzz ";
+			else print $i." ";
 	
+		}
 	}
 
 }
