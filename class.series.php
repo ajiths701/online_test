73a4fe02b52d5cf4e46564479321c599c6b4b7e7
Ajith Sebastian <ajiths701@gmail.com>
2012-11-18 17:38:57 +0800

    Created new method to dispay FizzBuzz series

---
+++ class.series.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Date 			: 18 Nov, 2012
+Author			: Ajith Sebastian.
+Purpose			: PHP class handles multple methods to display different series.
+Class name 		: Fizz Buzz Series class
+Version			: version 1.00 
+
+*/
+
+class number_series{
+
+	
+	public function fizzBuzzSeries(){
+	
+	}
+
+}
